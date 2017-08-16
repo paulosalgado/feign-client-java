efdb0ff1e5519d2830da09779548b91ad60beced
Paulo José de Oliveira Salgado <paulo@technosoftware.com.br>
2017-08-16 12:36:40 -0300

    criação do projeto.

---
+++ src/main/java/com/paulosalgado/GreetingClient.java
@@ -0,0 +1,12 @@
+package com.paulosalgado;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@FeignClient("spring-cloud-eureka-client")
+public interface GreetingClient {
+	
+	@RequestMapping("/greeting")
+	String greeting();
+	
+}
